f967314c051b53a54320b094cbdd98a6ec583f0d
tzs123 <68800227+tzs123@users.noreply.github.com>
2022-07-29 20:43:28 +0800

    Delete test123.py

--- test123.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# _*_ coding:utf-8 _*_
-
-
-def test2():
-    for i in range(1, 10):
-        for j in range(1, i + 1):
-            print('{}x{}={}\t'.format(j, i, i * j), end='')
-    print()
